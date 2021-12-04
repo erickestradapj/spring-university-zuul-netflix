127c43796594d64925de272787fabc97dfcb99a6
Erick Estrada <erickestradapj@gmail.com>
2021-12-04 16:55:22 -0500

    starting project

---
+++ src/test/java/com/dev/microservices/app/zuul/ZuulNetflixApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dev.microservices.app.zuul;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZuulNetflixApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
