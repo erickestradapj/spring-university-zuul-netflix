2000133428b37989eaf76ab52df5ac73fccd7f4f
Erick Estrada <erickestradapj@gmail.com>
2021-12-04 19:07:26 -0500

    properties added

--- src/main/java/com/dev/microservices/app/zuul/ZuulNetflixApplication.java
+++ src/main/java/com/dev/microservices/app/zuul/ZuulNetflixApplication.java
@@ -2,12 +2,16 @@ package com.dev.microservices.app.zuul;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
+@EnableZuulProxy
+@EnableEurekaClient
 @SpringBootApplication
 public class ZuulNetflixApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ZuulNetflixApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(ZuulNetflixApplication.class, args);
+    }
 
 }
